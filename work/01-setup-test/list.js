69e8e07cbee6f58aaa4c302d277e6d81eb054ae7
hsiaohcc01 <hsiaohcc01@gmail.com>
2024-01-08 13:52:51 -0700

    Adds Hsiao-Ching Chen

--- work/01-setup-test/list.js
+++ work/01-setup-test/list.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 const people = `
-Name           |  NEUID   | Email                     | Slack handle           | github username
-Brett Ritter   | ???      | b.ritter@northeastern.edu | @Brett Ritter (He/Him) | swiftone
+Name             |   NEUID   | Email                      | Slack handle                | github username
+Hsiao-Ching Chen | 002749406 | chen.hsia@northeastern.edu | @Hsiao-Ching Chen (She/Her) | hsiaohcc01
 
 `.split('\n') // convert to array of lines
 .filter( line => !!line.replace(/\s/g,'' )); // Remove empty lines
