974fe466e299806524898f0c1dab63156c60f874
hsiaohcc01 <hsiaohcc01@gmail.com>
2024-01-22 17:33:46 -0700

    feat: compare two words

--- work/03-basic-js/compare.js
+++ work/03-basic-js/compare.js
@@ -2,9 +2,26 @@
 /* DO NOT MODIFY EXCEPT WHERE ALLOWED */
 module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
-function compare( word, guess ) {  // DO NOT MODIFY
+function compare(word, guess) { // DO NOT MODIFY
 
-/* YOU MAY MODIFY THE LINES BELOW */
+    /* YOU MAY MODIFY THE LINES BELOW */
 
-  return 0; // this line is wrong
-}
+    /* 1. Convert words to lowercase */
+    word = word.toLowerCase();
+    guess = guess.toLowerCase();
+    const uniqueLetters = new Set(word);
+    let count = 0;
+
+    /* Compare uniqueLetters(word) and letter(guess) */
+    for (let letter of guess) {
+        /* If match then count+1 and delete the word in case duplicates */
+        if (uniqueLetters.has(letter)) {
+            count++;
+            uniqueLetters.delete(letter);
+        }
+    }
+
+    /* Log the total count of matching letters
+    console.log("Total match count: " + matchCount);*/
+    return count;
+  }
